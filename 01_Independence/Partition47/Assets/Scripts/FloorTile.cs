a80750188ac30c8873fab51f350c3fd8380937f9
umairazfar <umairazfar@gmail.com>
2017-08-09 00:14:22 +0500

    Add files via upload

---
+++ 01_Independence/Partition47/Assets/Scripts/FloorTile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorTile : MonoBehaviour {
+
+    bool occupied = false;
+
+    public bool IsOccupied()
+    {
+        return occupied;
+    }
+
+    public void SetOccupied(bool occ)
+    {
+        occupied = occ;
+    }
+}
